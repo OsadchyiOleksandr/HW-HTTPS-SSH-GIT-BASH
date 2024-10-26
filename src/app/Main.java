40d6c6bb7761e8a933db48852975772c0f229952
Oleksandr Osadchyi <osadchyi.oleksandr@protonmail.com>
2024-10-26 18:32:31 +0100

    Initial commit. Add functional for dislpaing info

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+import app.core.View;
+
+public class Main {
+    public static void main(String[] args) {
+        View.toDisplay("This App converting \"miles to km\" and \"km to miles\"");
+    }
+}
---
+++ src/app/core/View.java
@@ -0,0 +1,7 @@
+package app.core;
+
+public class View {
+    public static void toDisplay(String message){
+        System.out.println(message);
+    }
+}
